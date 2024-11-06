bf6f11699e41f191cbe33b7cc81eaeac371c3f29
Rachit <rachitbhatt2000@gmail.com>
2024-11-05 19:30:50 -0500

    Removed and re-created environment.

--- CityPulse/src/app/app.component.spec.ts
+++ CityPulse/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'city-pulse' title`, () => {
+  it(`should have the 'CityPulse' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('city-pulse');
+    expect(app.title).toEqual('CityPulse');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, city-pulse');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CityPulse');
   });
 });
