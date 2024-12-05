9d123acb1ff594961ba33505bdb4ad6c05a38189
Rachit <rachitbhatt2000@gmail.com>
2024-12-05 16:51:31 -0500

    Added pages in the UI.

--- city-pulse/src/app/app.component.ts
+++ city-pulse/src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { DateTimeComponent } from './MyComponents/date-time/date-time.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { OurServicesComponent } from './MyComponents/our-services/our-services.component';
+import { OurCustomersComponent } from './MyComponents/our-customers/our-customers.component';
+import { AboutUsComponent } from './MyComponents/about-us/about-us.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, DateTimeComponent, RouterModule],
+  imports: [CommonModule, DateTimeComponent, RouterModule, OurServicesComponent, OurCustomersComponent, AboutUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
